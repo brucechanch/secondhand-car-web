625d62e941399d97fdc71c00ccbd1d814ad5ba97
brucechanch <brucechanch@gmail.com>
2023-12-04 17:00:17 +0800

    Header complete

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,17 @@ import Home from './pages/Home'
 import About from './pages/About'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
-
+import Header from './components/Header'
 
 
 function App() {
 return <BrowserRouter>
+<Header />
 <Routes>
 <Route path="/" element={<Home />} />
 <Route path="/about" element={<About />} />
 <Route path="/sign-in" element={<SignIn />} />
 <Route path="sign-up" element={<SignUp />} />
-
 </Routes>
 </BrowserRouter>
 }
